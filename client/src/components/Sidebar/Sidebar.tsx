29f76b07f0765c09742c9ba5bc11796809496e48
beasroy <roybipasha676@gmail.com>
2024-03-31 17:40:22 +0530

    home page ready

---
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+const Sidebar: React.FC = () => {
+  return (
+    <div className="h-full w-2/5 p-5">
+     
+        <h1 className="text-3xl p-4">File Structure</h1>
+
+    </div>
+  );
+};
+
+export default Sidebar;
+
